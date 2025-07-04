6c6a6249e7b7ebe2805043b02249d8d9e8cf5147
ediewow25 <ediewow@25gmail.com>
2025-07-04 18:11:26 +0800

    Added nuxt auth plugins

--- pages/auth/signin.vue
+++ pages/auth/signin.vue
@@ -1,70 +1,24 @@
 <template>
   <div class="d-flex align-center justify-center" style="height: 100vh;">
+        <v-card width="500" class="px-8 py-10 textcenter">
+        <h1 class="d-flex align-cneter justify-center">LOG IN PAGE</h1>
+        <v-form>
+        <v-text-field label="Email" outlined prepend-inner-icon="mdi-email"></v-text-field>
+        <v-text-field label="Password" outlined prepend-inner-icon="mdi-lock"></v-text-field>
 
-    <template>
-  <v-sheet class="bg-deep-purple pa-12" rounded>
-    <v-card class="mx-auto px-6 py-8" max-width="400">
-      <v-form
-        v-model="form"
-        @submit.prevent="onSubmit"
-      >
-        <v-text-field
-          v-model="email"
-          :readonly="loading"
-          :rules="[required]"
-          class="mb-2"
-          label="Email"
-          clearable
-        ></v-text-field>
+        <v-btn color="primary" rounded block> log in </v-btn>
+        <div class="d-flex align-center justify-center">or</div>
+        <v-btn color="error" class="text-capitalize" rounded block><v-icon left>mdi-google</v-icon>sign in with google</v-btn>
 
-        <v-text-field
-          v-model="password"
-          :readonly="loading"
-          :rules="[required]"
-          label="Password"
-          placeholder="Enter your password"
-          clearable
-        ></v-text-field>
+        
+    </v-form>
 
-        <br>
-
-        <v-btn
-          :disabled="!form"
-          :loading="loading"
-          color="success"
-          size="large"
-          type="submit"
-          variant="elevated"
-          block
-        >
-          Sign In
-        </v-btn>
-      </v-form>
-    </v-card>
-  </v-sheet>
-</template>
-<script setup>
-  import { ref } from 'vue'
-
-  const form = ref(false)
-  const email = ref(null)
-  const password = ref(null)
-  const loading = ref(false)
-
-  function onSubmit () {
-    if (!form.value) return
-    loading.value = true
-    setTimeout(() => (loading.value = false), 2000)
-  }
-  function required (v) {
-    return !!v || 'Field is required'
-  }
-</script>
-  </div>
+       </v-card>
+     </div>
 </template>
 <script>
 export default {
-    layout: "auth"
+     layout: "auth"
 }
 </script>
     
