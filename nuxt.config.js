18d6a5461006a6b5a191da1dfd150d836bb057c6
ediewow25 <ediewow@25gmail.com>
2025-07-28 16:26:29 +0800

    tapos na

--- nuxt.config.js
+++ nuxt.config.js
@@ -69,7 +69,7 @@ export default {
       },
       responseType: "token id_token",
       scope: ["openid", "profile", "email"],
-      redirectUri: "http://localhost:3000/auth/callback",
+      redirectUri: process.env.REDIRECT_URI,
       codeChallengeMethod: "",
     },
   }
