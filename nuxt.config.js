726c71079b2d4c9c3207d8e1ad200bf1da181135
unknown <yvetteesguerra5@gmail.com>
2025-07-30 12:25:20 +0800

    rerey

--- nuxt.config.js
+++ nuxt.config.js
@@ -56,7 +56,7 @@ export default {
     autoFetchUser: false,
   strategies: {
     google: {
-      clientId: process.env.GOOGLE_ID,
+      clientId: process.env.GOOGLE_CLIENT_ID ||"280183468761-l1jb6ias2641s8ada0f35pl9tferut2v.apps.googleusercontent.com",
       scheme: "oauth2",
       endpoint: {
         authorization: "https://accounts.google.com/o/oauth2/auth",
@@ -69,7 +69,7 @@ export default {
       },
       responseType: "token id_token",
       scope: ["openid", "profile", "email"],
-      redirectUri: process.env.REDIRECT_URI,
+      redirectUri: process.env.GOOGLE_REDIRECT_URI || "http://localhost:3000/auth/callback",
       codeChallengeMethod: "",
     },
   }
